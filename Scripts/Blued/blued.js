0c2cae4e886b87916503a557c77cea1c7e5c6d65
HerbertGao <heapcn@gmail.com>
2023-01-03 13:47:48 +0800

    feat: blued-stash

--- Scripts/Blued/blued.js
+++ Scripts/Blued/blued.js
@@ -1,24 +1,88 @@
 const url = $request.url;
 let headers = $request.headers;
 
-function notify(title = "", subtitle = "", content = "", open_url) {
-    let opts = {};
-    if (open_url) opts["open-url"] = open_url;
-    if (JSON.stringify(opts) === "{}") {
-        $notify(title, subtitle, content);
-    } else {
-        $notify(title, subtitle, content, opts);
-
+function tool() {
+    const isSurge = typeof $httpClient != "undefined"
+    const isQuanX = typeof $task != "undefined"
+    const isResponse = typeof $response != "undefined"
+    const node = (() => {
+        if (typeof require == "function") {
+            const request = require('request')
+            return ({ request })
+        } else {
+            return (null)
+        }
+    })()
+    const notify = (title, subtitle, message) => {
+        if (isQuanX) $notify(title, subtitle, message)
+        if (isSurge) $notification.post(title, subtitle, message)
+        if (node) console.log(JSON.stringify({ title, subtitle, message }));
     }
+    const write = (value, key) => {
+        if (isQuanX) return $prefs.setValueForKey(value, key)
+        if (isSurge) return $persistentStore.write(value, key)
+    }
+    const read = (key) => {
+        if (isQuanX) return $prefs.valueForKey(key)
+        if (isSurge) return $persistentStore.read(key)
+    }
+    const adapterStatus = (response) => {
+        if (response) {
+            if (response.status) {
+                response["statusCode"] = response.status
+            } else if (response.statusCode) {
+                response["status"] = response.statusCode
+            }
+        }
+        return response
+    }
+    const get = (options, callback) => {
+        if (isQuanX) {
+            if (typeof options == "string") options = { url: options }
+            options["method"] = "GET"
+            $task.fetch(options).then(response => {
+                callback(null, adapterStatus(response), response.body)
+            }, reason => callback(reason.error, null, null))
+        }
+        if (isSurge) $httpClient.get(options, (error, response, body) => {
+            callback(error, adapterStatus(response), body)
+        })
+        if (node) {
+            node.request(options, (error, response, body) => {
+                callback(error, adapterStatus(response), body)
+            })
+        }
+    }
+    const post = (options, callback) => {
+        if (isQuanX) {
+            if (typeof options == "string") options = { url: options }
+            options["method"] = "POST"
+            $task.fetch(options).then(response => {
+                callback(null, adapterStatus(response), response.body)
+            }, reason => callback(reason.error, null, null))
+        }
+        if (isSurge) {
+            $httpClient.post(options, (error, response, body) => {
+                callback(error, adapterStatus(response), body)
+            })
+        }
+        if (node) {
+            node.request.post(options, (error, response, body) => {
+                callback(error, adapterStatus(response), body)
+            })
+        }
+    }
+    return { isQuanX, isSurge, isResponse, notify, write, read, get, post }
 }
+
 
 async function launch() {
     if (headers['User-Agent'].indexOf("Blued") !== -1) {
-        notify("🐔", "点击跳转到浏览器打开看图", url, url);
+        notify("🐔", "点击跳转到浏览器打开看图", url);
         console.log(url)
     }
     if (headers['User-Agent'].indexOf("Media") !== -1) {
-        notify("🐔", "点击跳转到浏览器打开看图", url, url);
+        notify("🐔", "点击跳转到浏览器打开看图", url);
         console.log(url)
     }
     $done();
